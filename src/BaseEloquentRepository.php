c666eff141097c9f56917ce897fea74091112f9d
Giorgos Kalmoukis <giorgoskalmoukis@gmail.com>
2023-02-18 01:39:53 +0200

    Refactor base repository

---
+++ src/BaseEloquentRepository.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Gkalmoukis\Repositories;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+abstract class BaseEloquentRepository
+{
+    public function __construct(
+        protected Model $model
+    ) {}
+
+
+    public function all(array $relations = []) 
+    {
+        return $this->model
+            ->with($relations)
+            ->get();
+    }
+
+    public function allPaginated(array $relations = []) : LengthAwarePaginator 
+    {
+        return $this->model
+            ->with($relations)
+            ->paginate();
+    }
+
+    public function store(array $attributes)
+    {
+        return $this->model
+            ->create($attributes);
+    }
+
+    public function modify($id, array $attributes)
+    {
+        return $this->model
+            ->findOrFail($id)
+            ->update($attributes);
+    }
+
+    public function delete($id)
+    {
+        return $this->model
+            ->findOrFail($id)
+            ->delete();
+    }
+
+    public function byId($id, $relations = [])
+    {
+        return $this->model
+            ->with($relations)
+            ->findOrFail($id);
+    }
+   
+}
